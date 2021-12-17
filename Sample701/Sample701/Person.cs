e056cae9a2bb95445594cecaba06d9957bf1544b
katomikiya <kato-m@omoteya.co.jp>
2021-12-17 11:14:44 +0900

    プロパティ（セッターとゲッター）

---
+++ Sample701/Sample701/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample701
+{
+    internal class Person
+    {
+        private string name = "";
+        private int age = 0;
+
+        public void setAgeAndName(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public void showAgeAndName()
+        {
+            Console.WriteLine("名前：{0} 年齢：{1}", name, age);
+        }
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+        public int Age
+        {
+            set { age = value; }
+            get { return age; }
+        }
+    }
+}
---
+++ Sample701/Sample701/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample701
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person per1 = new Person();
+            Person per2 = new Person();
+
+            per1.Name = "山田太郎";
+            per1.Age = 19;
+            per2.setAgeAndName("佐藤花子", 23);
+
+            per1.showAgeAndName();
+            Console.WriteLine("名前：{0} 年齢：{1}", per2.Name, per2.Age);
+        }
+    }
+}
