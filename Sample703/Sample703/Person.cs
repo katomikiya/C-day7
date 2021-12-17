ada617eebeca2dd4575f9b4d2d0e5f8d0a54644d
katomikiya <kato-m@omoteya.co.jp>
2021-12-17 13:11:25 +0900

    自動実装プロパティ

---
+++ Sample703/Sample703/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample703
+{
+    internal class Person
+    {
+        public void setAgeAndName(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public void showAgeAndName()
+        {
+            Console.WriteLine("名前：{0} 年齢：{1}", Name, Age);
+        }
+
+        public string Name
+        {
+            private set; get;
+        }
+        public int Age
+        {
+            set; get;
+        }
+    }
+}
---
+++ Sample703/Sample703/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample703
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person per = new Person();
+
+            per.setAgeAndName("山田太郎", 26);
+            per.Age = 32;
+
+            per.showAgeAndName();
+            
+        }
+    }
+}
